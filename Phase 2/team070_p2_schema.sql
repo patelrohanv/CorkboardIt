076b7fb4a7481f4dd5de58bc7eb4ca992dca8508
David Tsui <david@Davids-MBP.fios-router.home>
2018-10-09 03:51:29 -0400

    Updated create tables to match with postgres syntax as well as eer mapped relations

--- Phase 2/team070_p2_schema.sql
+++ Phase 2/team070_p2_schema.sql
@@ -1,50 +1,70 @@
 -- Tables
-
-CREATE TABLE `User` (
-    email varchar(250) NOT NULL,
-    pin varchar(60) NOT NULL,
-    first_name varchar(100) NOT NULL,
-    last_name varchar(100) NOT NULL,
-    PRIMARY KEY (email),
-);
-
-CREATE TABLE `Comment` (
-    user_email varchar(250) NOT NULL,
-    date_time datetime NOT NULL,
-    comment_text varchar(1000) NOT NULL,
-    -- Some way to connect to pushpin
-    PRIMARY KEY (user_email,date_time),
-);
-
-CREATE TABLE 'PushPin' (
-    description varchar(250) NOT NULL,
-    url varchar(250) NOT NULL,
-    date_time datetime NOT NULL,
-    user_email varchar(250) NOT NULL
-    corkboard_title varchar(250) NOT NULL
-    PRIMARY KEY (user_email, corkboard_title, date_time),
-);
-
-CREATE TABLE 'PushPinTags' (
-    pushpin_date_time datetime NOT NULL,
-    user_email varchar(250) NOT NULL,
-    corkboard_title varchar(250) NOT NULL,
-    tag varchar(250) NOT NULL
-    PRIMARY KEY (pushpin_date_time, user_email, corkboard_title)
+CREATE TABLE CorkBoardItUser (
+    user_id SERIAL PRIMARY KEY ,
+    email VARCHAR(250) NOT NULL,
+    pin NUMERIC(4) NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE CorkBoard (
-    user_email varchar(250) NOT NULL,
-    date_time datetime NOT NULL,
-    title varchar(250) NOT NULL,
-    visability varchar(250) NOT NULL,
-    password varchar(250),
-    PRIMARY KEY (user_email, title),
+    corkboard_id SERIAL PRIMARY KEY,
+    fk_user_id INTEGER REFERENCES CorkBoardItUser(user_id),
+    email VARCHAR(250) NOT NULL,
+    date_time_posted TIMESTAMP NOT NULL,
+    title VARCHAR(250) NOT NULL,
+    visibility VARCHAR(250) NOT NULL
 );
 
-CREATE TABLE 'CorkBoardCategories' (
-    user_email varchar(250) NOT NULL,
-    corkboard_title varchar(250) NOT NULL,
-    category varchar(250) NOT NULL
-    PRIMARY KEY (user_email, corkboard_title)
+CREATE TABLE PublicCorkBoard (
+    public_corkboard_id SERIAL PRIMARY KEY,
+    fk_corkboard_id INTEGER REFERENCES CorkBoard(corkboard_id)
+);
+
+CREATE TABLE PrivateCorkBoard (
+    private_corkboard_id SERIAL PRIMARY KEY,
+    fk_corkboard_id INTEGER REFERENCES CorkBoard(corkboard_id),
+    password VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE Category (
+    fk_corkboard_id INTEGER REFERENCES CorkBoard(corkboard_id),
+    category VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE PushPin (
+    pushpin_id SERIAL PRIMARY KEY,
+    fk_user_id INTEGER REFERENCES CorkBoardItUser(user_id),
+    fk_corkboard_id INTEGER REFERENCES CorkBoard(corkboard_id),
+    date_time_posted TIMESTAMP NOT NULL,
+    url VARCHAR(250) NOT NULL,
+    description(250) NULL
+);
+
+CREATE TABLE Tag (
+    fk_pushpin_id INTEGER REFERENCES PushPin(pushpin_id),
+    tag VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE Comment (
+    comment_id SERIAL PRIMARY KEY,
+    date_time_posted TIMESTAMP NOT NULL,
+    text VARCHAR(250) NOT NULL,
+    fk_user_id INTEGER REFERENCES CorkBoardItUser(user_id),
+    fk_pushpin_id INTEGER REFERENCES PushPin(pushpin_id)
+);
+
+CREATE TABLE Watch (
+    fk_user_id INTEGER REFERENCES CorkBoardItUser(user_id),
+    fk_public_corkboard_id INTEGER REFERENCES PublicCorkBoard(public_corkboard_id)
+);
+
+CREATE TABLE Like (
+    fk_user_id INTEGER REFERENCES CorkBoardItUser(user_id),
+    fk_pushpin_id INTEGER REFERENCES PushPin(pushpin_id)
+);
+
+CREATE TABLE Follow (
+    fk_user_follower_id INTEGER REFERENCES CorkBoardItUser(user_id),
+    fk_user_followee_id INTEGER REFERENCES CorkBoardItUser(user_id)
 );
