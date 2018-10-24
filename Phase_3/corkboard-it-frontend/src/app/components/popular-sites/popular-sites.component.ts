f250fda3ca4938c94c729d8122ae3e2fae03b727
jmachamer3 <julie.machamer@gmail.com>
2018-10-24 18:48:50 -0400

    adding to popular sites

--- Phase_3/corkboard-it-frontend/src/app/components/popular-sites/popular-sites.component.ts
+++ Phase_3/corkboard-it-frontend/src/app/components/popular-sites/popular-sites.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PopularSites {
+  site: string;
+  push_pins: number;
+}
+
 @Component({
   selector: 'app-popular-sites',
   templateUrl: './popular-sites.component.html',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopularSitesComponent implements OnInit {
 
+  // vars
+  popular_sites : PopularSites[];
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  getPopularSites(){
+    //  todo make api call to get popular tags
+    //  todo load into PopularSites obj
+    //  todo display data
+
+    //  dummy data for now
+    this.popular_sites =  [
+      {site: 'www.google.com', push_pins: 4},
+      {site:'www.gatech.edu', push_pins: 5 },
+      {site: 'www.weather.com', push_pins: 2},
+      {site : 'www.reddit.com', push_pins:10}
+    ];
+
+  }
+
 
 }
