f887ee84a3d239b7bcf3bd9069fb07c9f9c0c151
rohan Patel <rpatel475@gatech.edu>
2018-10-24 21:41:49 -0400

    fixed typo

--- Phase_3/corkboard-it-frontend/src/app/app.component.ts
+++ Phase_3/corkboard-it-frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
-import { LoginComponent } from './components/login';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
